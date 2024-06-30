965fd839bcdf57c385f23fcd2e4d0030431eb839
Mike Bruce <mikepjb@proton.me>
2024-06-30 20:50:27 +0100

    release candidate

--- init.lua
+++ init.lua
@@ -1,39 +1,36 @@
 -- Editor Configuration
 -- optional plugins: plenary + nvim-telescope/telescope.nvim ------------------
+-- clone the plugins here: ~/.config/nvim/pack/base/start/ --------------------
 
 local base = vim.api.nvim_create_augroup('Base', {})
 
 -- basic configuration --------------------------------------------------------
-vim.opt.guicursor = ""
-
+vim.opt.guicursor = ""        -- a. visual config
+vim.opt.cursorline = true
+vim.opt.textwidth = 79
+vim.opt.colorcolumn = '80'
 vim.opt.nu = true
+vim.opt.showtabline = 2
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 50
 
-vim.opt.tabstop = 4
+vim.opt.tabstop = 4            -- b. indentation
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
-vim.opt.cursorline = true
-vim.opt.colorcolumn = '80'
 vim.opt.smartindent = true
-vim.opt.gdefault = true
-
 vim.opt.wrap = false
 
-vim.opt.swapfile = false
+vim.opt.swapfile = false       -- c. backups
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
 vim.opt.undofile = true
 
+vim.opt.gdefault = true        -- d. search/replacment
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-
-vim.opt.updatetime = 50
 
 -- keybindings ----------------------------------------------------------------
 vim.keymap.set("n", "gn", ":tabnew ~/.notes/src/SUMMARY.md<CR>")
@@ -47,20 +44,21 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("i", "<C-l>", " => ")
 vim.keymap.set("i", "<C-u>", " -> ")
 
--- TODO include inside nvim grepping function?
+vim.keymap.set("n", "gr", function ()
+    if vim.g.loaded_telescope == 1 then
+        return ":Telescope live_grep<CR>"
+    else
+        return ":grep "
+    end
+end, { expr = true })
 
 vim.keymap.set("n", "<space>", function ()
-    -- if nvim-telescope/telescope.nvim & plenary.nvim are installed
-    -- clone the plugins here: ~/.config/nvim/pack/base/start/
     if vim.g.loaded_telescope == 1 then
-        vim.cmd('Telescope find_files')
-        -- TODO can we do this directly without .cmd?
+        return "Telescope find_files"
     else
-        print('no telescope')
-        -- we want :find
+        return ":find "
     end
-end)
-
+end, { expr = true })
 
 -- LSP ------------------------------------------------------------------------
 function register_lsp(cmd, pattern, root_files)
@@ -83,9 +81,11 @@ register_lsp({'typescript-language-server', '--stdio'},
              'javascript,typescript,javascriptreact, typescriptreact',
              {'package.json'})
 
--- LspAttach!
-
--- vim.fn.colorscheme('retrobox')
+-- the rest? ------------------------------------------------------------------
+local ok, _ = pcall(vim.cmd, 'colorscheme retrobox')
+if not ok then
+  vim.cmd 'colorscheme default' -- if the above fails, then use default
+end
 
 -- TODO include tmux too!
 local bashrc = [[
@@ -99,6 +99,17 @@ PS1='\W($(git branch --show-current 2>/dev/null || echo "!")) \$ '
 local git_config = {
     ["core.editor"] = "nvim",
     ["core.autocrlf"] = "false",
+    ["init.defaultBranch"] = "main",
+    ["alias.aa"] = "add --all",
+    ["alias.br"] = "branch --sort=committerdate",
+    ["alias.st"] = "status",
+    ["alias.up"] = "pull --rebase",
+    ["alias.co"] = "checkout",
+    ["alias.ci"] = "commit --verbose",
+    ["alias.di"] = "diff",
+    ["alias.push-new"] = "push -u origin HEAD",
+    ["alias.ra"] = "log --pretty=format:" ..
+    "\"%C(yellow)%h%Creset %<(7,trunc)%ae%C(auto)%d %Creset%s %Cgreen(%cr)\""
 }
 
 function dots()
@@ -106,7 +117,7 @@ function dots()
     vim.fn.writefile(vim.fn.split(bashrc, "\n"), vim.fn.expand("$HOME/.bashrc"))
     if vim.fn.executable("git") == 1 then
         for k, v in pairs(git_config) do
-            print("git config --global --replace-all " .. k .. " '" .. v .. "'")
+            vim.fn.system("git config --global --replace-all " .. k .. " '" .. v .. "'")
         end
     end
 end
