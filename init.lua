2232b32e18020f487cbc642057cb53910702f30e
Mike Bruce <mike.bruce@loveholidays.com>
2025-01-09 10:45:07 +0000

    include lspconfig (incl. java) but config is huge

--- init.lua
+++ init.lua
@@ -10,7 +10,7 @@
 --     end
 --     return nil
 -- end
--- 
+--
 -- local telescope = maybe_require("telescope.builtin")
 -- if telescope then
 --     vim.keymap.set('n', '<space>', builtin.find_files, {})
@@ -18,14 +18,14 @@
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -36,81 +36,155 @@ vim.opt.rtp:prepend(lazypath)
 -- TODO do not use lazy.nvim since you can't resource :so your config
 -- Initialize lazy with plugins
 require("lazy").setup({
-  {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.5',
-    dependencies = { 
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+        config = function()
+            -- Basic Telescope keymaps
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<space>', builtin.find_files, {})
+            -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            -- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        end,
     },
-    config = function()
-      -- Basic Telescope keymaps
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<space>', builtin.find_files, {})
-      -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      -- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end,
-  },
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("tokyonight").setup({
-            transparent = true,  -- Enable transparency
-            styles = {
-                sidebars = "transparent",
-                floats = "transparent",
-            },
-        })
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+    {
+        'folke/tokyonight.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                transparent = true, -- Enable transparency
+                styles = {
+                    sidebars = "transparent",
+                    floats = "transparent",
+                },
+            })
+            vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
+        config = function()
+            -- Setup Mason to automatically install LSP servers
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                automatic_installation = true,
+                ensure_installed = {
+                    "lua_ls",
+                    "gopls",
+                    "pyright",
+                    "ts_ls",
+                    "rust_analyzer",
+                    "jdtls",
+                },
+            })
+
+            -- Format on save function
+            local format_on_save = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ buffer = bufnr })
+                        end,
+                    })
+                end
+            end
+
+            -- LSP attach function
+            local on_attach = function(client, bufnr)
+                -- Enable format on save
+                format_on_save(client, bufnr)
+
+                -- Keymaps
+                local opts = { noremap = true, silent = true, buffer = bufnr }
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+            end
+
+            -- LSP server configurations
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+
+            -- Lua
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {
+                    Lua = {
+                        diagnostics = { globals = { "vim" } },
+                    },
+                },
+            })
+
+            -- Other LSPs with default settings
+            local servers = { "gopls", "pyright", "ts_ls", "rust_analyzer", "jdtls" }
+            for _, lsp in ipairs(servers) do
+                lspconfig[lsp].setup({
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                })
+            end
+        end,
+    },
 })
 
 local base = vim.api.nvim_create_augroup('Base', { clear = true })
 
 -- editor configuration ---------------------------------------------------------------------------
-vim.opt.guicursor = ""        -- a. visual config
+vim.opt.guicursor = "" -- a. visual config
 vim.opt.cursorline = true
 vim.opt.textwidth = 99
 vim.opt.colorcolumn = '100'
 vim.opt.spell = false -- no spell, can we enable for just markdown + comments?
 vim.opt.nu = true
-vim.opt.rnu = false -- maybe default true in future?
+vim.opt.rnu = false   -- maybe default true in future?
 vim.opt.showtabline = 2
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 vim.opt.wildmode = "list:longest,list:full"
-vim.opt.wildignore:append({"node_modules"})
-vim.opt.suffixesadd:append({".rs"}) -- search for suffixes using gf
-vim.opt.completeopt:remove("preview") -- no preview buffer during completion
-vim.opt.clipboard:append({"unnamedplus"}) -- integrate with system clipboard
+vim.opt.wildignore:append({ "node_modules" })
+vim.opt.suffixesadd:append({ ".rs" })       -- search for suffixes using gf
+vim.opt.completeopt:remove("preview")       -- no preview buffer during completion
+vim.opt.clipboard:append({ "unnamedplus" }) -- integrate with system clipboard
 vim.opt.autoread = true
-vim.opt.foldlevel = 1 -- by default, only show top level fold/heading
+vim.opt.foldlevel = 1                       -- by default, only show top level fold/heading
 
-vim.opt.tabstop = 4            -- b. indentation
+vim.opt.tabstop = 4                         -- b. indentation
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
 
-vim.opt.swapfile = false       -- c. backups
+vim.opt.swapfile = false -- c. backups
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
 vim.opt.undofile = true
 
-vim.opt.gdefault = true        -- d. search/replacment
+vim.opt.gdefault = true -- d. search/replacment
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.inccommand = "split"
 
-vim.g.markdown_fenced_languages = {'typescript', 'javascript', 'bash', 'go'}
+vim.g.markdown_fenced_languages = { 'typescript', 'javascript', 'bash', 'go' }
 vim.g.omni_sql_no_default_maps = 1 -- don't use C-c for autocompletion in SQL.
 
 vim.opt.termguicolors = os.getenv("COLORTERM") == 'truecolor'
@@ -123,7 +197,7 @@ vim.opt.termguicolors = os.getenv("COLORTERM") == 'truecolor'
 
 -- keymap configuration ---------------------------------------------------------------------------
 
-function set_path_to_git_root(filepath) -- or do nothing if not in git.
+local function set_path_to_git_root(filepath) -- or do nothing if not in git.
     if not filepath then
         filepath = vim.fn.expand("%:p:h")
     end
@@ -143,7 +217,7 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 vim.keymap.set("n", "<C-g>", ":noh<CR><C-g>")
 vim.keymap.set("n", "<Tab>", "<C-^>")
-vim.keymap.set("n", "L", function () vim.diagnostic.open_float(0, {scope="line"}) end)
+vim.keymap.set("n", "L", function() vim.diagnostic.open_float(0, { scope = "line" }) end)
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("i", "<C-l>", " => ")
 vim.keymap.set("i", "<C-u>", " -> ")
@@ -151,20 +225,21 @@ vim.keymap.set("t", "<C-g>", "<C-\\><C-n>")
 vim.keymap.set("n", "<C-t>", ":tabnew<CR>")
 vim.keymap.set("n", "gn", ":tabnew ~/notes/src/index.md<CR>")
 vim.keymap.set("n", "go", ":tabnew ~/notes/src/ops.md<CR>")
-vim.keymap.set("n", "g0", function () vim.lsp.stop_client(vim.lsp.get_active_clients()) end)
+vim.keymap.set("n", "g0", function() vim.lsp.stop_client(vim.lsp.get_active_clients()) end)
 vim.keymap.set("n", "gl", ":set rnu!<CR>") -- toggle relative line number
 vim.keymap.set("n", "g?", ":Dots<CR>")
 vim.keymap.set("n", "gi", ":tabnew ~/.config/nvim/init.lua<CR>")
 vim.keymap.set("n", "gp", ":call feedkeys(':tabnew<space>~/src/<tab>', 't')<CR>")
 vim.keymap.set("n", "gP", set_path_to_git_root) -- backup, shouldn't need to do this manually.
 vim.keymap.set("n", "gr", ":call feedkeys(':grep<space>', 't')<CR>")
-vim.keymap.set("n", "g*", function () vim.cmd(":grep " .. vim.fn.expand("<cword>")) end)
+vim.keymap.set("n", "g*", function() vim.cmd(":grep " .. vim.fn.expand("<cword>")) end)
 vim.keymap.set("n", ",", "/TODO\\|NEXT\\|XXX<CR>")
 vim.keymap.set("n", "-", "za")
 vim.keymap.set("n", "_", ":set foldlevel=1<CR>")
 
 vim.api.nvim_create_autocmd("TabNewEntered", {
-    group = base, callback = function(ev)
+    group = base,
+    callback = function(ev)
         vim.schedule(function()
             local netrw_dir = vim.b.netrw_curdir
             if netrw_dir then
@@ -177,9 +252,9 @@ vim.api.nvim_create_autocmd("TabNewEntered", {
 -- always open quickfix window automatically.
 -- this uses cwindows which will open it only if there are entries.
 vim.api.nvim_create_autocmd("QuickFixCmdPost", {
-  group = vim.api.nvim_create_augroup("AutoOpenQuickfix", { clear = true }),
-  pattern = { "[^l]*" },
-  command = "cwindow"
+    group = vim.api.nvim_create_augroup("AutoOpenQuickfix", { clear = true }),
+    pattern = { "[^l]*" },
+    command = "cwindow"
 })
 
 local css_reset = [[
@@ -220,70 +295,70 @@ end
 --     end
 -- end, { expr = true })
 
-function register_lsp(cmd, pattern, root_files)
-    vim.api.nvim_create_autocmd("FileType", {
-        pattern = pattern,
-        callback = function()
-            local root_dir = vim.fs.dirname(
-                vim.fs.find(root_files, { upward = true })[1]
-            )
-            local client = vim.lsp.start({
-                name = cmd[1], cmd = cmd, root_dir = root_dir,
-            })
-            if vim.fn.executable(cmd[1]) == 1 then
-                vim.lsp.buf_attach_client(0, client)
-            else
-               print("Could not register lsp, '" .. cmd[0] .. "' couldn't be found.") 
-            end
-        end
-    })
-end
-
-register_lsp({'rust-analyzer'}, 'rust', {'Cargo.toml'})
--- register_lsp({'jdtls'}, 'java', {'gradlew'})
-
-if vim.fn.executable("typescript-language-server") == 1 and
-    vim.fn.isdirectory("./node_modules/typescript") == 1 then
-    register_lsp(
-        {'typescript-language-server', '--stdio'},
-        'javascript,typescript,javascriptreact,typescriptreact',
-        {'package.json'}
-    )
-end
-
-register_lsp({"gopls"}, "go", {"go.mod"})
--- register_lsp({"golangci-lint"}, "go", {"go.mod"})
-
+-- function register_lsp(cmd, pattern, root_files)
+--     vim.api.nvim_create_autocmd("FileType", {
+--         pattern = pattern,
+--         callback = function()
+--             local root_dir = vim.fs.dirname(
+--                 vim.fs.find(root_files, { upward = true })[1]
+--             )
+--             local client = vim.lsp.start({
+--                 name = cmd[1], cmd = cmd, root_dir = root_dir,
+--             })
+--             if vim.fn.executable(cmd[1]) == 1 then
+--                 vim.lsp.buf_attach_client(0, client)
+--             else
+--                print("Could not register lsp, '" .. cmd[0] .. "' couldn't be found.")
+--             end
+--         end
+--     })
+-- end
 --
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = base,
-    callback = function()
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    end
-})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    local params = vim.lsp.util.make_range_params()
-    params.context = {only = {"source.organizeImports"}}
-    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
-    -- machine and codebase, you may want longer. Add an additional
-    -- argument after params if you find that you have to write the file
-    -- twice for changes to be saved.
-    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-    for cid, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-          vim.lsp.util.apply_workspace_edit(r.edit, enc)
-        end
-      end
-    end
-    vim.lsp.buf.format({async = false})
-  end
-})
+-- register_lsp({'rust-analyzer'}, 'rust', {'Cargo.toml'})
+-- -- register_lsp({'jdtls'}, 'java', {'gradlew'})
+--
+-- if vim.fn.executable("typescript-language-server") == 1 and
+--     vim.fn.isdirectory("./node_modules/typescript") == 1 then
+--     register_lsp(
+--         {'typescript-language-server', '--stdio'},
+--         'javascript,typescript,javascriptreact,typescriptreact',
+--         {'package.json'}
+--     )
+-- end
+--
+-- register_lsp({"gopls"}, "go", {"go.mod"})
+-- -- register_lsp({"golangci-lint"}, "go", {"go.mod"})
+--
+-- --
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--     group = base,
+--     callback = function()
+--         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+--     end
+-- })
+--
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.go",
+--   callback = function()
+--     local params = vim.lsp.util.make_range_params()
+--     params.context = {only = {"source.organizeImports"}}
+--     -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+--     -- machine and codebase, you may want longer. Add an additional
+--     -- argument after params if you find that you have to write the file
+--     -- twice for changes to be saved.
+--     -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+--     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+--     for cid, res in pairs(result or {}) do
+--       for _, r in pairs(res.result or {}) do
+--         if r.edit then
+--           local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+--           vim.lsp.util.apply_workspace_edit(r.edit, enc)
+--         end
+--       end
+--     end
+--     vim.lsp.buf.format({async = false})
+--   end
+-- })
 
 local bashrc = [=[
 [[ -f /etc/bash_completion ]] && . /etc/bash_completion
@@ -315,11 +390,11 @@ local git_config = {
     ["alias.count"] = "shortlog -sn",
     ["alias.push-new"] = "push -u origin HEAD",
     ["alias.ra"] = "log --pretty=format:" ..
-    "\"%C(yellow)%h%Creset %<(7,trunc)%ae%C(auto)%d %Creset%s %Cgreen(%cr)\""
+        "\"%C(yellow)%h%Creset %<(7,trunc)%ae%C(auto)%d %Creset%s %Cgreen(%cr)\""
 }
 
 function dots()
-    vim.fn.writefile({". ~/.bashrc"}, vim.fn.expand("$HOME/.bash_profile"))
+    vim.fn.writefile({ ". ~/.bashrc" }, vim.fn.expand("$HOME/.bash_profile"))
     vim.fn.writefile(vim.fn.split(bashrc, "\n"), vim.fn.expand("$HOME/.bashrc"))
     vim.fn.writefile(vim.fn.split(tmux_conf, "\n"), vim.fn.expand("$HOME/.config/tmux/tmux.conf"))
     if vim.fn.executable("git") == 1 then
