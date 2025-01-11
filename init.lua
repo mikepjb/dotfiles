7d4e595bd46553d4106c17c52e85c75bdcf24544
Mike Bruce <mike@hypalynx.com>
2025-01-11 02:18:57 +0000

    update tmux config

--- init.lua
+++ init.lua
@@ -111,8 +111,8 @@ require("lazy").setup({
                 local opts = { noremap = true, silent = true, buffer = bufnr }
                 vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-                vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
+                -- vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+                -- vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
                 vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
             end
 
