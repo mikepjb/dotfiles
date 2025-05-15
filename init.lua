a9b313ae9c5a8159e989139b2b31ef8371ec8e59
Mike Bruce <mike@hypalynx.com>
2025-05-15 11:18:24 +0100

    java lsp for neovim

--- init.lua
+++ init.lua
@@ -314,8 +314,9 @@ if lspconfig and mason and mason_lspconfig then
             "ts_ls",
             "rust_analyzer",
             "html",
-            "htmx",
             "tailwindcss",
+            "htmx",
+            "jdtls",
         },
     })
 
@@ -375,6 +376,129 @@ if lspconfig and mason and mason_lspconfig then
             on_attach = on_attach,
         })
     end
+
+    local function jdk_source()
+        local java_home = os.getenv('JAVA_HOME')
+        local jdk_src = java_home .. '/lib/src.zip'
+
+        if vim.fn.filereadable(jdk_src) == 1 then
+            return jdk_src
+        else
+            -- Try to find source.zip or src.zip in various locations
+            local possible_paths = {
+                java_home .. '/src.zip',
+                java_home .. '/lib/src.zip',
+                java_home .. '/jre/lib/src.zip',
+                -- macOS-specific paths
+                java_home .. '/../src.zip',
+                '/Library/Java/JavaVirtualMachines/adoptopenjdk-*/Contents/Home/src.zip',
+            }
+
+            for _, path in ipairs(possible_paths) do
+                local expanded_path = vim.fn.glob(path)
+                if expanded_path ~= '' and vim.fn.filereadable(expanded_path) == 1 then
+                    return expanded_path
+                end
+            end
+        end
+
+        return nil
+    end
+
+    local jdtls_path = vim.fn.stdpath('data') .. '/mason/packages/jdtls'
+
+    local function get_mason_launcher_jar()
+        local launcher_jar = vim.fn.glob(jdtls_path .. '/plugins/org.eclipse.equinox.launcher_*.jar')
+
+        if launcher_jar == '' then
+            error('Could not find launcher jar in Mason. Please install JDTLS properly.')
+            return nil
+        end
+
+        return launcher_jar
+    end
+
+    local os_config
+    if vim.fn.has('mac') == 1 then
+        os_config = 'config_mac'
+    elseif vim.fn.has('unix') == 1 then
+        os_config = 'config_linux'
+    elseif vim.fn.has('win32') == 1 then
+        os_config = 'config_win'
+    end
+
+    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+    local workspace_dir = vim.fn.expand('~/.cache/jdtls/workspace/') .. project_name
+
+
+    lspconfig.jdtls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        filetypes = { "java" },
+        cmd = {
+            'java',
+            '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+            '-Dosgi.bundles.defaultStartLevel=4',
+            '-Declipse.product=org.eclipse.jdt.ls.core.product',
+            '-Dlog.protocol=true',
+            '-Dlog.level=ALL',
+            '-Xmx1G',
+            '--add-modules=ALL-SYSTEM',
+            '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+            '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+            '-jar', get_mason_launcher_jar(),
+            '-configuration', jdtls_path .. '/' .. os_config,
+            '-javaagent:' .. jdtls_path .. '/lombok.jar',  -- Important for Lombok and Spring Boot annotation processing
+            '--add-opens=java.base/java.lang=ALL-UNNAMED', -- This is critical for Spring Boot '@' annotations
+            '-data', workspace_dir,
+        },
+        settings = {
+            java = {
+                signatureHelp = { enabled = true },
+                contentProvider = { preferred = 'fernflower' },
+                completion = {
+                    importOrder = {
+                        "java",
+                        "javax",
+                        "com",
+                        "org",
+                        "io",
+                        ""
+                    },
+                },
+                maven = { downloadSources = true },
+                gradle = { downloadSources = true },
+                implementationsCodeLens = { enabled = true },
+                referencesCodeLens = { enabled = true },
+                references = { includeDecompiledSources = true },
+                inlayHints = { parameterNames = { enabled = "all" } },
+                sources = {
+                    organizeImports = {
+                        starThreshold = 9999,
+                        staticStarThreshold = 9999,
+                    },
+                },
+                codeGeneration = {
+                    toString = {
+                        template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+                    },
+                    useBlocks = true,
+                },
+                configuration = {
+                    updateBuildConfiguration = "interactive",
+                    runtimes = {
+                        {
+                            name = "JavaSE-21",
+                            sources = jdk_source(),
+                            javaHome = os.getenv('JAVA_HOME'),
+                            path = os.getenv("HOME") .. "/.sdkman/candidates/java/current",
+
+                        },
+                    },
+                },
+            }
+        },
+    })
 
     lspconfig.html.setup({
         on_attach = on_attach,
